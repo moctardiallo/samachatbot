33702979e8172b701bdb255231ce135df9b48710
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2018-12-18 03:27:57 +0000

    Initial commit from React&Flask by mj

---
+++ samachatter/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h3>A chat App</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ samachatter/App.py
@@ -0,0 +1,13 @@
+from samachatter import app, api
+from flask_restful import Resource
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
