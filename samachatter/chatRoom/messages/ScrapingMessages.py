118e4f5f6ff6ad60c8b014cf877ddaf6a4b0c65c
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2018-12-26 02:01:46 +0000

    Delete scraper

--- samachatter/chatRoom/messages/ScrapingMessages.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-from bs4 import BeautifulSoup
-
-fb = requests.get('https://web.facebook.com/groups/1618790341747413/').text
-
-page = BeautifulSoup(fb, 'lxml')
-
-print(page.prettify())
